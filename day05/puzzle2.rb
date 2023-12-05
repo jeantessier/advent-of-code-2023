3ce84e7f7074eee37fc58d497695f9d90f8c2701
Jean Tessier <jean@jeantessier.com>
2023-12-05 02:50:08 -0800

    Second attempt at Day 05 Puzzle 2
    Breakdown ranges based on mapping functions.  Looks for gaps that might
    fall between mapped ranges and that use identity mapping.
    
    Runs real fast.

--- day05/puzzle2.rb
+++ day05/puzzle2.rb
@@ -1,7 +1,8 @@
 #!/usr/bin/env ruby
 
 # lines = File.readlines("sample.txt") # Answer: 46
-lines = File.readlines("input.txt") # Answer:
+# lines = File.readlines("modified_sample.txt") # Answer: 20
+lines = File.readlines("input.txt") # Answer: 11554135
 
 # Parse the seeds
 seed_ranges = lines
@@ -25,7 +26,7 @@ puts seed_ranges
 
 # Parse all the maps
 
-puts ""
+# puts ""
 
 maps = []
 
@@ -37,7 +38,7 @@ lines.each do |line|
   when /(\w+)\-to\-(\w+) map/
     source = $1
     destination = $2
-    puts "Start new maps from #{source} to #{destination}"
+    # puts "Start new maps from #{source} to #{destination}"
     current_map = {}
     current_map.default_proc = lambda { |hash, key| hash[key] = key }
     maps << {
@@ -49,16 +50,9 @@ lines.each do |line|
     destination_range_start = $1.to_i
     source_range_start = $2.to_i
     range_length = $3.to_i
-    # puts "destination_range_start: #{destination_range_start}"
-    # puts "source_range_start: #{source_range_start}"
-    # puts "range_length: #{range_length}"
-    # (0...(range_length.to_i)).each do |i|
-    #   # puts "#{source_range_start.to_i + i} --> #{destination_range_start.to_i + i}"
-    #   current_map[source_range_start.to_i + i] = destination_range_start.to_i + i
-    # end
     source_range = source_range_start...(source_range_start + range_length)
     destination_range = destination_range_start...(destination_range_start + range_length)
-    puts "#{source_range} --> #{destination_range}"
+    # puts "#{source_range} --> #{destination_range}"
     current_map[source_range] = lambda {|n| destination_range_start + n - source_range_start}
   end
 end
@@ -69,32 +63,119 @@ puts "----"
 puts maps
 
 puts ""
-puts "Resolving Seeds"
-puts "---------------"
+puts "Resolving Seed Ranges"
+puts "---------------------"
 
-def resolve(maps, source, value)
+def resolve(maps, source, source_ranges)
+  puts ""
+  puts "Resolving #{source}"
   map = maps.find {|m| m[:source] == source}
-  return value unless map
+  return source_ranges unless map
 
-  mapper = map[:map].find {|range, mapper| range.include?(value)}
+  mappings = source_ranges.map do |source_range|
 
-  resolve(maps, map[:destination], mapper ? mapper[1].call(value) : value)
-end
+    # Mappings from mapped conversions
+    mapped_ranges = map[:map].map do |range, mapper|
+      ranges_are_overlapping = (
+        range.cover?(source_range.min) or
+        range.cover?(source_range.max) or
+        source_range.cover?(range.min) or
+        source_range.cover?(range.max)
+      )
 
-locations = seed_ranges.map do |seed_range|
-  puts "Seeds #{seed_range}"
-  seed_range.reduce(1_000_000_000) do |memo, seed|
-    location = resolve(maps, "seed", seed)
-    location < memo ? location : memo
+      if ranges_are_overlapping
+        source_range_start = [source_range.min, range.min].max
+        source_range_end = [source_range.max, range.max].min
+
+        destination_range_start = mapper.call(source_range_start)
+        destination_range_end = mapper.call(source_range_end)
+
+        puts "  #{source} #{source_range_start..source_range_end} --> #{map[:destination]} #{destination_range_start..destination_range_end}"
+        {
+          source => source_range_start..source_range_end,
+          map[:destination] => destination_range_start..destination_range_end,
+        }
+      else
+        nil
+      end
+    end.filter do |range|
+      range
+    end.sort do |a, b|
+      a[source].min <=> b[source].min
+    end
+
+    puts "#{source} #{source_range} has mappings #{mapped_ranges}" unless mapped_ranges.empty?
+
+    # Fill out gaps with identity mappings
+    pos = source_range.min
+    gaps = []
+    mapped_ranges.map do |mapped_range|
+      mapped_range[source]
+    end.each do |subrange|
+      if pos < subrange.min
+        gap = pos...(subrange.min)
+        puts "  #{source} #{gap} --> #{map[:destination]} #{gap}"
+        gaps << {
+          source => gap,
+          map[:destination] => gap,
+        }
+      end
+      pos = subrange.max + 1
+    end
+
+    if pos < source_range.max
+      gap = pos..(source_range.max)
+      puts "  #{source} #{gap} --> #{map[:destination]} #{gap}"
+      gaps << {
+        source => gap,
+        map[:destination] => gap,
+      }
+    end
+
+    puts "#{source} #{source_range} has gaps #{gaps}" unless gaps.empty?
+
+    result = (mapped_ranges + gaps).sort do |a, b|
+      a[source].min <=> b[source].min
+    end
+
+    puts "#{source} #{source_range} totals #{result}"
+
+    result
+  end.flatten
+
+  puts ""
+  puts "Mappings"
+  puts "--------"
+  puts mappings
+
+  destination_mappings = mappings.map do |mapped_range|
+    mapped_range[map[:destination]]
+  end.map do |range|
+    (range.min)..(range.max)
+  end.sort do |a, b|
+    a.min <=> b.min
   end
+
+  puts ""
+  puts "#{map[:destination]} mappings"
+  puts "-----------------"
+  puts destination_mappings
+
+  resolve(maps, map[:destination], destination_mappings)
+  # destination_mappings
+
 end
+
+locations = resolve(maps, "seed", seed_ranges)
 
 puts ""
 puts "Locations"
 puts "---------"
-puts locations
+locations.each do |range|
+  puts "#{range} (#{range.size})"
+end
 
 puts ""
 puts "Answer"
 puts "------"
-puts locations.min
+puts locations.map {|r| r.min}.min
