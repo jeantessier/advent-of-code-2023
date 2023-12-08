99e449e46b43836835f9c91c28ffe49046288b55
Jean Tessier <jean@jeantessier.com>
2023-12-07 22:29:35 -0800

    Second attempt at Day 08 Puzzle 2
    Doing all the paths in sync and looping until they all terminate was
    taking too long.  Each path reaches a termination in ~10-20k steps.
    There might be something with common factors.
    
    Just multiplying them together, as if they were pure cycles, didn't
    help.  I need to look for cycles within each path and then figure out
    how long until they all converge.

--- day08/puzzle2.rb
+++ day08/puzzle2.rb
@@ -2,7 +2,7 @@
 
 # lines = readlines
 # lines = File.readlines("sample2.txt") # Answer: 6
-lines = File.readlines("input.txt") # Answer:
+lines = File.readlines("input.txt") # Answer: 34002592904266863793513273
 
 map = lines
   .map {|line| /(?<node>\w+)\s*=\s*\((?<left>\w+),\s*(?<right>\w+)\)/.match(line)}
@@ -26,26 +26,46 @@ puts "Instructions"
 puts "------------"
 puts instructions
 
-current_nodes = map.keys.select {|node| node =~ /..A/}
+starting_nodes = map.keys.select {|node| node =~ /..A/}
 
 puts ""
 puts "Starting Nodes"
 puts "--------------"
-puts current_nodes
+puts starting_nodes
+
+def navigate(map, instructions, starting_node)
+  current_node = starting_node
+  answer = 0
+  instructions.cycle.each_with_index do |instruction, i|
+    next_node = map[current_node][instruction]
+    # puts "#{i}: #{current_node} + #{instruction} --> #{next_node}"
+    current_node = next_node
+    if current_node =~ /..Z/
+      # puts ""
+      # puts "Answer"
+      # puts "------"
+      answer = i + 1
+      puts "#{starting_node} --> #{current_node}: #{answer}"
+      break
+    end
+  end
+
+  answer
+end
 
 puts ""
-puts "Navigation"
-puts "----------"
-
-instructions.cycle.each_with_index do |instruction, i|
-  next_nodes = current_nodes.collect {|node| map[node][instruction]}
-  puts "#{i}: #{current_nodes} + #{instruction} --> #{next_nodes}"
-  current_nodes = next_nodes
-  if current_nodes.all? {|node| node =~ /..Z/}
-    puts ""
-    puts "Answer"
-    puts "------"
-    puts i + 1
-    break
-  end
+puts "Navigations"
+puts "-----------"
+cycle_lengths = starting_nodes.collect do |node|
+  navigate(map, instructions, node)
 end
+
+puts ""
+puts "Cycle Lengths"
+puts "-------------"
+puts cycle_lengths
+
+puts ""
+puts "Answer"
+puts "------"
+puts cycle_lengths.reduce(:*)
