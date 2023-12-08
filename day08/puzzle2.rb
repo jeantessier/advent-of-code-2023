3671eeb25f40ff17669ca3e9f0301de8e558d6af
Jean Tessier <jean@jeantessier.com>
2023-12-08 00:24:53 -0800

    Third attempt at Day 08 Puzzle 2
    I had to verify that each path eventually looped back upon itself.
    There are 6 starting points and each one is a full cycle that
    immediately starts over itself.  So the paths are not too complex.  Now,
    I need to extract common factors to get the correct convergence.
    
    Ruby's `prime` library to the rescue!!
    
    Luckily, all path lengths are prime multiples of 293.  The solution
    becomes the product of all prime factors and 293.

--- day08/puzzle2.rb
+++ day08/puzzle2.rb
@@ -1,8 +1,10 @@
 #!/usr/bin/env ruby
 
+require 'prime'
+
 # lines = readlines
 # lines = File.readlines("sample2.txt") # Answer: 6
-lines = File.readlines("input.txt") # Answer: 34002592904266863793513273
+lines = File.readlines("input.txt") # Answer: 15746133679061
 
 map = lines
   .map {|line| /(?<node>\w+)\s*=\s*\((?<left>\w+),\s*(?<right>\w+)\)/.match(line)}
@@ -65,7 +67,54 @@ puts "Cycle Lengths"
 puts "-------------"
 puts cycle_lengths
 
+factorized_cycle_lengths = cycle_lengths.map {|cycle_length| cycle_length.prime_division}
+
+puts ""
+puts "Factorized Cycle Lengths"
+puts "------------------------"
+factorized_cycle_lengths.each do |primes|
+  puts primes.to_s
+end
+
+histo = {}
+histo.default_proc = lambda {|hash, key| hash[key] = []}
+factorized_cycle_lengths.reduce(histo) do |memo, factors|
+  factors.each do |pair|
+    memo[pair[0]] << pair[1]
+  end
+  memo
+end
+
+puts ""
+puts "Histo"
+puts "-----"
+puts histo
+
+common_factors = histo.select do |factor, counts|
+  counts.size == factorized_cycle_lengths.size
+end.map do |factor, counts|
+  [factor, counts.min]
+end.to_h
+
+common_factors.default = 0
+
+puts ""
+puts "Common Factors"
+puts "--------------"
+puts common_factors.to_s
+
+remaining_factors = factorized_cycle_lengths.map do |factors|
+  factors.map do |pair|
+    pair[0] ** (pair[1] - common_factors[pair[0]])
+  end
+end.flatten
+
+puts ""
+puts "Remaining Factors"
+puts "-----------------"
+puts remaining_factors.to_s
+
 puts ""
 puts "Answer"
 puts "------"
-puts cycle_lengths.reduce(:*)
+puts (common_factors.map {|factor, power| factor ** power} + remaining_factors).reduce(:*)
